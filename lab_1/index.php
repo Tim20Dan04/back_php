bccf2b3317289e9483af7f8cea56de0b38ee1811
Danilov Timofey <timofey.danilov@gmail.com>
2024-02-13 13:01:13 +0300

    lab_1

---
+++ lab_1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Danilov_231-322</title>
+    </head>
+    <body>
+        <?php 
+            echo "<h2>Hello everybody, it is my first PHP File</h2>"; 
+        ?> 
+    </body>
+</html>
+
+
+
+<!-- 
+floor округляет в меньшую сторону
+round округляет арифметически
+ceil - округляет в большую сторону 
+abs возвращает модуль числа (Math.abs)
+pow возводит число в указанную степень (Math.pow)
+log возвращает натуральный логарифм числа (Math.log) 
+-->
